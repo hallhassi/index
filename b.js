633b8f26471a5549c271355679dfc24ad0777a58
hallhassi <hallhassi@gmail.com>
2022-05-05 01:53:32 -0500

    redesign

---
+++ b.js
@@ -0,0 +1,53 @@
+const b = () => {
+const fs = require('fs');
+let years = [];
+for (let i=2008; i<2023; i++) {
+  years.push(i.toString());
+}
+const navdef = years.map(i => `<a href="${i}/index.html">${i}</a>`);
+for (let year of years) {
+  const nav = years.map(i => i == year ? i : `<a href="../${i}/index.html">${i}</a>`);
+  const defs = ['index', '404'];
+  for (let def of defs) {
+    fs.promises.writeFile(`${def}.html`, `<!-- blaiselarmee@gmail.com -->
+    <html>
+    <head>
+      <title>${def}</title> 
+    </head>
+    <body>
+      <nav>
+      ${navdef.join(' ')}
+      </nav>
+      <main>
+      </main>
+    </body>
+    </html>`)  
+  }
+fs.promises
+  .readdir(year)
+  .then(filenames => {
+    let aList = [];
+    for (let filename of filenames) {
+      if (/\.html/.test(filename) || filename == '.DS_Store') {}
+      else if (/\.\w+$/.test(filename)) {
+        aList.push(`<a href='${filename}'>${filename}</a>`);
+      } else {}
+    }
+    fs.promises.writeFile(year + `/index.html`, `<!-- blaiselarmee@gmail.com -->
+    <html>
+    <head>
+      <title>${year}</title> 
+    </head>
+    <body>
+      <nav>
+      ${nav.join(' ')}
+      </nav>
+      <main>
+      ${aList.join('<br>')}
+      </main>
+    </body>
+    </html>`)
+  });
+  }
+}
+exports.b = b();
---
+++ t.js
@@ -0,0 +1,21 @@
+const { resolve } = require('path');
+const { readdir } = require('fs').promises;
+
+async function* getFiles(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* getFiles(res);
+    } else {
+      yield res;
+    }
+  }
+}
+
+
+;(async () => {
+  for await (const f of getFiles('.')) {
+    console.log(f);
+  }
+})()
